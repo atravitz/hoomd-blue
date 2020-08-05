149694be3842ae202e436491cb8ef4415880e25f
Alyssa Travitz <atravitz@umich.edu>
2020-08-05 11:12:13 -0400

    removing correlator link for now for simplicity

--- hoomdbuild.sh
+++ hoomdbuild.sh
@@ -18,15 +18,14 @@ echo "Using compilers $($CC --version | head -n 1), $($CXX --version | head -n 1
 CMAKE_FLAGS="${CMAKE_FLAGS} -DPYTHON_EXECUTABLE=$(which python)"
 PYTHON_LIBRARY_PATH=$(python -c "import distutils.sysconfig as sysconfig; import os; print(os.path.join(sysconfig.get_config_var('LIBDIR'), sysconfig.get_config_var('LDLIBRARY')))")
 CMAKE_FLAGS="${CMAKE_FLAGS} -DPYTHON_LIBRARY=${PYTHON_LIBRARY_PATH}"  # -DCMAKE_BUILD_TYPE=Debug"
-CMAKE_FLAGS="${CMAKE_FLAGS} -DCMAKE_BUILD_TYPE=release -DBUILD_HPMC=OFF -DBUILD_METAL=OFF -DBUILD_CGCMM=OFF -DENABLE_MPI=OFF"
+CMAKE_FLAGS="${CMAKE_FLAGS} -DCMAKE_BUILD_TYPE=debug -DBUILD_HPMC=OFF -DBUILD_METAL=OFF -DBUILD_CGCMM=OFF -DENABLE_MPI=OFF"
 # Install to the conda packages path
 CMAKE_FLAGS="${CMAKE_FLAGS} -DCMAKE_INSTALL_PREFIX=/Users/atravitz/popbd-test-env/lib/python3.8/site-packages" #${CONDA_PREFIX}/lib/python3.8/site-packages"
 
 # link correlator
-cd ../hoomd 
-ln -s ../correlator-hoomd-plugin/correlator .
-
-cd ../build
+# cd ../hoomd 
+# ln -s ../correlator-hoomd-plugin/correlator .
+# cd ../build
 
 cmake ../ ${CMAKE_FLAGS} 
 make -j4  
